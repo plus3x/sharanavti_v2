fede55697bf0f68011ba31a46c7c06842c6b8bab
plus3x <electronicchest@gmail.com>
2014-02-06 15:48:48 +0400

    Add alias of find method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,42 @@
 class User < ActiveResource::Base
+
   # schema do
   #   integer :id
   #   string :email, :name
   #   string :password
   # end
   
-  def self.new(attributes = self.get(:new))
-    super attributes
-  end
-  
-  def self.find(id)
-    GameAPI.user_find_by_id( id: id ) rescue nil
-  end
-  
-  def self.find_by_email(email)
-    validate email
-    GameAPI.user_find_by_email( email: email ) rescue nil
+  class << self
+
+    def new(attributes = self.get(:new))
+      super attributes
+    end
+
+    def find_by(hash)
+      case hash.keys[0]
+      when :id
+        find_by_id hash.values[0]
+      when :email
+        find_by_email hash.values[0]
+      else
+        nil
+      end
+    end
+
+    def find_by_id(id)
+      GameAPI.user_find_by_id( id: id ) rescue nil
+    end
+    alias_method :find, :find_by_id
+
+    def find_by_email(email)
+      validate email
+      GameAPI.user_find_by_email( email: email ) rescue nil
+    end
+
   end
   
   protected
-  
+
     def validate(email)
       errors.add(:email, 'not avalible format') unless email.match(/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/)
     end
